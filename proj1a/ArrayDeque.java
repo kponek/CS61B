7b159cf7be986cd049847d7e6660b022a461a1c2
Kevin Ponek <Kevin Ponek>
2017-02-02 01:25:39 -0800

    place holder ArrayDeque

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,35 @@
+/**
+ * Created by kevin on 1/30/2017.
+ */
+public class ArrayDeque<Item> {
+    private Item sentinel;
+    private int size;
+
+    public ArrayDeque(){
+
+    }
+    public void addFirst(Item){
+
+    }
+    public void addLast(Item){
+
+    }
+    public boolean isEmpty(){
+
+    }
+    public int size(){
+        return size;
+    }
+    public void printDeque(){
+
+    }
+    public Item removeFirst(){
+        return sentinel;
+    }
+    public Item removeLast(){
+        return sentinel;
+    }
+    public Item get(int index){
+        return sentinel;
+    }
+}
