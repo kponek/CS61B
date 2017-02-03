342299211cd45ac039d311280f9cdc8510d38e3c
Kevin Ponek <Kevin Ponek>
2017-02-02 16:30:50 -0800

    Project 1a 2/2/17 at 4:30pm

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -2,34 +2,92 @@
  * Created by kevin on 1/30/2017.
  */
 public class ArrayDeque<Item> {
-    private Item sentinel;
+    private Item[] items;
     private int size;
+    private int nextFirst;
+    private int nextLast;
 
-    public ArrayDeque(){
+    /** Initialize new ArrayDeque with 8 spaces and nothing inside */
+    public ArrayDeque() {
+        items = (Item []) new Object[8];
+        size = 0;
+        nextFirst = 0;
+        nextLast = 1;
+    }
 
+    /** Adds an item to the front of the deque. Special case when the next first
+     *  is already at the 0 index: move nextFirst to the end at length - 1 index*/
+    public void addFirst(Item newFirst) {
+        items[nextFirst] = newFirst;
+        if (nextFirst == 0) {
+            nextFirst = items.length - 1;
+        }
+        else {
+            nextFirst--;
+        }
     }
-    public void addFirst(Item newFirst){
-
+    /** Adds an item to the back of the deque. Special case when the last is
+     *  already at the length - 1 index: move nextLast to the beginning at 0 index */
+    public void addLast(Item newLast) {
+        items[nextLast] = newLast;
+        if (nextLast == items.length - 1) {
+            nextLast = 0;
+        }
+        else {
+            nextLast++;
+        }
     }
-    public void addLast(Item newLast){
-
+    /** Checks if the deque is empty. True if size is not 0, otherwise false. */
+    public boolean isEmpty() {
+        return (size == 0);
     }
-    public boolean isEmpty(){
-        return false;
-    }
-    public int size(){
+    /** Returns the size fo the deque */
+    public int size() {
         return size;
     }
-    public void printDeque(){
-
+    /** Prints the deque starting at the nextFirst pointer + 1, and ending at
+     *  the nextLast pointer. */
+    public void printDeque() {
+        int pointer = nextFirst + 1;
+        int end = nextLast;
+        if (pointer == items.length - 1) {
+            pointer = 0;
+        }
+        while (pointer != end) {
+            System.out.print(items[pointer]);
+            if (pointer == items.length - 1) {
+                pointer = 0;
+            }
+            else {
+                pointer++;
+            }
+        }
     }
-    public Item removeFirst(){
-        return sentinel;
+    /** Removes (aka moves the nextFirst pointer) the first item of the deque
+     *  Special case when the nextFirst pointer is at the end, then move it to index 0.
+     *  Return the removed item.*/
+    public Item removeFirst() {
+        if (nextFirst == items.length - 1) {
+            nextFirst = 0;
+        }
+        else {
+            nextFirst++;
+        }
+        return items[nextFirst];
     }
-    public Item removeLast(){
-        return sentinel;
+    /** Removes (aka moves the nextLast pointer) the last item of the deque
+     *  Special case when the nextLast pointer is at the beginning, then move it
+     *  to index length - 1. Return the removed item.*/
+    public Item removeLast() {
+        if (nextLast == 0) {
+            nextLast = items.length - 1;
+        }
+        else {
+            nextLast--;
+        }
+        return items[nextLast];
     }
-    public Item get(int index){
-        return sentinel;
+    public Item get(int index) {
+        return items[index];
     }
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -5,81 +5,93 @@ public class LinkedListDeque<Item> {
     private StuffNode sentinel;
     private int size;
 
-    public class StuffNode{
-        public Item item;
-        public StuffNode next;
-        public StuffNode prev;
+    private class StuffNode {
+        private Item item;
+        private StuffNode next;
+        private StuffNode prev;
 
-        public StuffNode(Item i, StuffNode n, StuffNode p){
+        public StuffNode(Item i, StuffNode n, StuffNode p) {
             item = i;
             next = n;
             prev = p;
         }
-
+        public Item getItem() {
+            return item;
+        }
+        public StuffNode getNext() {
+            return next;
+        }
+        public StuffNode getPrev() {
+            return prev;
+        }
     }
 
-    public LinkedListDeque(){
+    public LinkedListDeque() {
         size = 0;
-        sentinel = new StuffNode(null,null,null);
+        sentinel = new StuffNode(null, null, null);
     }
-    public void addFirst(Item newFirst){
-        sentinel = new StuffNode(newFirst,sentinel,sentinel.prev);
+    public void addFirst(Item newFirst) {
+        sentinel = new StuffNode(newFirst, sentinel, sentinel.prev);
         sentinel.next.prev = sentinel;
         size++;
     }
-    public void addLast(Item newLast){
-        sentinel.prev.next = new StuffNode(newLast,sentinel,sentinel.prev.next);
+    public void addLast(Item newLast) {
+        //check if the DLList is empty
+        if (sentinel.prev != null) {
+            sentinel.prev = new StuffNode(newLast, sentinel, sentinel.prev.prev);
+        }
+        //otherwise the DLList is empty so just insert normally
+        else {
+            sentinel = new StuffNode(newLast,sentinel,sentinel);
+        }
         size++;
     }
-    public boolean isEmpty(){
-        if (sentinel == null){
+    public boolean isEmpty() {
+        if (sentinel.next == null) {
             return true;
         }
         return false;
     }
-    public int size(){
+    public int size() {
         return size;
     }
-    public void printDeque(){
+    public void printDeque() {
         StuffNode curr = sentinel;
-        while(curr != null){
-            System.out.print(curr);
+        while(curr != null) {
+            System.out.print(curr.getItem());
             curr = curr.next;
-            if (curr.next == sentinel){
-                break;
-            }
         }
     }
-    public Item removeFirst(){
+    public Item removeFirst() {
         Item removedItem = sentinel.item;
         sentinel.prev = sentinel.next.prev;
         sentinel.next.prev = sentinel.prev;
         sentinel = sentinel.next;
         return removedItem;
     }
-    public Item removeLast(){
+    public Item removeLast() {
         Item removedItem = sentinel.prev.item;
         sentinel.prev.prev.next = sentinel;
         sentinel.prev = sentinel.prev.prev;
         return removedItem;
     }
-    public Item get(int index){
+    public Item get(int index) {
         //int count = size;
         StuffNode curr = sentinel;
-        while (index > 0){
+        while (index > 0) {
             curr = sentinel.next;
             index--;
         }
         return curr.item;
     }
-    public Item getRecursive(int index){
-        return recursiveHelper(index,sentinel);
+    public Item getRecursive(int index) {
+        return recursiveHelper(index, sentinel);
 
     }
-    private Item recursiveHelper(int index, StuffNode curr){
-        if (index == 0){
+    private Item recursiveHelper(int index, StuffNode curr) {
+        if (index == 0) {
             return curr.item;
         }
-        return recursiveHelper(index--,curr.next);
+        return recursiveHelper(index--, curr.next);
     }
 }
