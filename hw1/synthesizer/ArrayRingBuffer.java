e4a0096c0a2a707236d54e08fa7ffe359920a222
Kevin Ponek <Kevin Ponek>
2017-02-20 17:29:49 -0800

    removed TODO comments

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -14,11 +14,6 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      * Create a new ArrayRingBuffer with the given capacity.
      */
     public ArrayRingBuffer(int capacity) {
-        // TODO: Create new array with capacity elements.
-        //       first, last, and fillCount should all be set to 0.
-        //       this.capacity should be set appropriately. Note that the local variable
-        //       here shadows the field we inherit from AbstractBoundedQueue, so
-        //       you'll need to use this.capacity to set the capacity.
         first = 0;
         last = 0;
         fillCount = 0;
@@ -33,7 +28,6 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      */
     @Override
     public void enqueue(T x) {
-        // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
         if (rb[last] != null) {
             throw new RuntimeException("Ring buffer overflow");
         } else {
@@ -49,7 +43,6 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      * covered Monday.
      */
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
         if (fillCount > 0) {
             T dequeued = rb[first];
             rb[first] = null;
@@ -65,14 +58,12 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      * Return oldest item, but don't remove it.
      */
     public T peek() {
-        // TODO: Return the first item. None of your instance variables should change.
         if (isEmpty()) {
-            throw new RuntimeException("")
+            throw new RuntimeException("Ring buffer empty - can't peek");
         }
         return rb[first];
     }
 
-    // TODO: When you get to part 5, implement the needed code to support iteration.
     private class BufferWizard implements Iterator<T> {
         private int notionOfWhereHeIs;
 
