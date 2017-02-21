0af15c4971e6b975bebd56ea111fc3d7ebaf90e2
Kevin Ponek <Kevin Ponek>
2017-02-20 17:31:11 -0800

    removed redundant public modifier in BufferWizard constructor

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -67,7 +67,7 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     private class BufferWizard implements Iterator<T> {
         private int notionOfWhereHeIs;
 
-        public BufferWizard() {
+        BufferWizard() {
             notionOfWhereHeIs = 0;
         }
 
