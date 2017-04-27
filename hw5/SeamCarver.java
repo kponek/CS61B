892fd51a738f3b5bbdb10988c5c9ef59dfa02f2d
Kevin Ponek <Kevin Ponek>
2017-04-26 22:27:17 -0700

    hw5 done?

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -1,6 +1,9 @@
 /**
  * Created by kevin on 4/26/2017.
  */
+
+import edu.princeton.cs.algs4.Picture;
+
 public class SeamCarver {
     private Picture pic;
     double[][] energies;
--- hw5/ShowSeams.java
+++ hw5/ShowSeams.java
@@ -2,14 +2,13 @@
  *  Compilation:  javac ShowSeams.java
  *  Execution:    java ShowSeams input.png
  *  Dependencies: SeamCarver.java SCUtility.java
- *                
+ *
  *  Read image from file specified as command-line argument. Show 3 images 
  *  original image as well as horizontal and vertical seams of that image.
  *
  *  % java ShowSeams HJoceanSmall.png
  *
  ******************************************************************************/
-
 import edu.princeton.cs.algs4.Picture;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -32,9 +31,9 @@ public class ShowSeams {
     public static void main(String[] args) {
         Picture picture = new Picture(args[0]);
         StdOut.printf("%d-by-%d image\n", picture.width(), picture.height());
-        picture.show();        
+        picture.show();
         SeamCarver sc = new SeamCarver(picture);
-        
+
         StdOut.printf("Displaying horizontal seam calculated.\n");
         showHorizontalSeam(sc);
 
